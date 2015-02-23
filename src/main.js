0eeb1d89e331e5e2f3992a0524eba90d89639620
slak44 <sylviu44@gmail.com>
2015-02-23 17:36:42 +0200

    Removed lol.js, organized and commented everything

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 function main() {
   manipulateDOM();
-  setDate();
   setTime();
   updateRedditKarma();
 }
 
+/*Async stuff is in separate functions.*/
 function manipulateDOM() {
   byId("dataPane").style.left = ($(window).width()/2) - 400 + "px";
   byId("dataPane").style.top = "0px";
@@ -24,6 +24,7 @@ function manipulateDOM() {
   addData("date", "01 January 1970", "p");
   addData("redditkarma", "", "pre");
   byId("name").innerHTML = settings.name;
+  setDate();
 
   addTabPre("player", "Player: NAME (pid PID)");
   addTabPre("matchMap", "Map: MAP_NAME");
@@ -36,6 +37,7 @@ function manipulateDOM() {
   byId("matchHistoryPane").appendChild(createButton("assets/back.png", "javascript:;", "Go Back"));
 }
 
+/*Directly add to the DOM.*/
 function addData(id, content, element) {
   var e = document.createElement(element);
   e.innerHTML = content;
@@ -43,7 +45,6 @@ function addData(id, content, element) {
   $(e).toggleClass("blockabsolute globalText");
   byId("dataPane").appendChild(e);
 }
-
 function addTabPre(id, content) {
   var pre = document.createElement("pre");
   pre.innerHTML = content;
@@ -52,11 +53,7 @@ function addTabPre(id, content) {
   byId("matchHistoryPane").appendChild(pre);
 }
 
-function setBlockAbsolute() {
-  var children = byId("matchHistoryPane").children;
-  for (var i = 0; i < children.length; i++) $(children[i]).toggleClass("blockabsolute");
-}
-
+/*Returns the element.*/
 function createButton(imagePath, href, preText) {
   var link = document.createElement('a');
   var text = link.appendChild(document.createElement('pre'));
@@ -69,29 +66,41 @@ function createButton(imagePath, href, preText) {
   return link;
 }
 
+/*Configures elements.*/
+function setBlockAbsolute() {
+  var children = byId("matchHistoryPane").children;
+  for (var i = 0; i < children.length; i++) $(children[i]).toggleClass("blockabsolute");
+}
 function setButtonPos(buttonOffset) {
   var buttons = document.getElementsByClassName('button');
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].style.top = i * (50/*Button height*/ + buttonOffset/*Space between btns*/) + "px";
   }
 }
-
-function byId(id) {
-  return document.getElementById(id);
+function setTime() {
+  var d = new Date();
+  var h = d.getHours();
+  var m = d.getMinutes();
+  if (m < 10) m = "0" + m;
+  if (h < 10) h = "0" + h;
+  var time = h + ":" + m;
+  byId("time").innerHTML = time;
+  setTimeout(setTime, 5000);
+}
+function setDate() {
+  var d = new Date();
+  var day = d.getDate();
+  var month = d.getMonth();
+  var year = d.getFullYear();
+  if (day < 10) day = "0" + day;
+  var date = day + " " + getMonthName(month) + " " + year;
+  byId("date").innerHTML = date;
 }
 
-function hideMainPane(boolean) {
-  if (boolean) {
-    byId("defaultPane").style.left = "0px";
-    byId("defaultPane").className = "";
-    $("#defaultPane").toggleClass("goLeft");
-  } else {
-    byId("defaultPane").style.left = -$(window).width() + "px";
-    byId("defaultPane").className = "";
-    $("#defaultPane").toggleClass("goRight");
-  }
-}
-
+/*
+Updates the on-screen karma every 7.5s by requesting reddit data.
+Connection indicator relies on this method.
+*/
 function updateRedditKarma() {
   $.getJSON('https://www.reddit.com/user/'+settings.redditUser+'/about.json?',
     function(data){
@@ -103,65 +112,122 @@ function updateRedditKarma() {
   setTimeout(updateRedditKarma, 7500);
 }
 
-function setTime() {
-  var d = new Date();
-  var h = d.getHours();
-  var m = d.getMinutes();
-  if (m < 10) m = "0" + m;
-  if (h < 10) h = "0" + h;
-  var time = h + ":" + m;
-  byId("time").innerHTML = time;
-  setTimeout(setTime, 5000);
+/*Wrapper.*/
+function byId(id) {
+  return document.getElementById(id);
 }
 
-function setDate() {
-  var d = new Date();
-  var day = d.getDate();
-  var month = d.getMonth();
-  var year = d.getFullYear();
-  if (day < 10) day = "0" + day;
-  var date = day + " " + getMonthName(month) + " " + year;
-  byId("date").innerHTML = date;
-}
-
-function getMonthName(monthnumeral) {
-  switch (monthnumeral) {
-  case 0:
-    return "January";
-    break;
-  case 1:
-    return "February";
-    break;
-  case 2:
-    return "March";
-    break;
-  case 3:
-    return "April";
-    break;
-  case 4:
-    return "May";
-    break;
-  case 5:
-    return "June";
-    break;
-  case 6:
-    return "July";
-    break;
-  case 7:
-    return "August";
-    break;
-  case 8:
-    return "September";
-    break;
-  case 9:
-    return "October";
-    break;
-  case 10:
-    return "November";
-    break;
-  case 11:
-    return "December";
-    break;
-  default: return "What did you do with this method?"
+/*Move divs around.*/
+function moveDiv(side/*Left=true or right=false*/, id) {
+  if (side) {
+    byId(id).style.left = "0px";
+    byId(id).className = "";
+    $("#"+id).toggleClass("goLeft");
+  } else {
+    byId(id).style.left = -$(window).width() + "px";
+    byId(id).className = "";
+    $("#"+id).toggleClass("goRight");
   }
+}
+
+/*Displays the match history div. Is called if/when the match data is stored.*/
+function displayMatch() {
+  setTabFields();
+  var tableWidth = $(window).width() - 5/*Right padding*/ - 450/*Info offset*/;
+  createTable(tableWidth);
+  addPlayers(tableWidth);
+}
+/*Sets the style of a 'td'.*/
+function setTableCellStyle(tableCell/*The HTML object*/, side/*Player color*/, nr/*What row is it on*/, size/*Space from one of the sides*/) {
+  var align, color;
+  if (side == "RED") {
+    align = "right";
+    color = "#FFAAAA";
+    tableCell.style.right = size + "px";
+  } else {
+    tableCell.style.left = size + "px";
+    align = "left";
+    color = "#AAAAFF"
+  }
+  tableCell.style.display = "block";
+  tableCell.style.position = "absolute";
+  tableCell.style.textAlign = align;
+  tableCell.style.color = color;
+  tableCell.style.top = (nr * (20/*Font size, px*/+2/*Offset between texts*/)) + "px";
+  tableCell.style.width = "200px";
+}
+/*Adds the players and their data to the table.*/
+function addPlayers(width) {
+  var playerNames = getPlayerNames();
+  var playerScores = getPlayerScores();
+  for (var i = 0; i < 5; i++) {
+    var newRow = document.createElement('tr');
+    $(newRow).toggleClass("blockabsolute");
+    newRow.style.width = width;
+
+    var pa = document.createElement('td');
+    var paScore = document.createElement('td');
+    var pbScore = document.createElement('td');
+    var pb = document.createElement('td');
+
+    pa.innerHTML = playerNames[i];
+    paScore.innerHTML = playerScores[i];
+    pbScore.innerHTML = playerScores[i+5];
+    pb.innerHTML = playerNames[i+5];
+
+    var scoreOffset = 5/*Base padding*/+200/*Size of name*/+5/*Padding from name*/;
+    setTableCellStyle(pa, "RED", i, 5);
+    setTableCellStyle(paScore, "RED", i, scoreOffset);
+    setTableCellStyle(pbScore, "BLUE", i, scoreOffset);
+    setTableCellStyle(pb, "BLUE", i, 5);
+
+    newRow.appendChild(pa);
+    newRow.appendChild(paScore);
+    newRow.appendChild(pbScore);
+    newRow.appendChild(pb);
+    byId("players").appendChild(newRow);
+  }
+}
+/*Configures the table.*/
+function createTable(width) {
+  var table = document.createElement("table");
+  $(table).toggleClass("blockabsolute");
+  table.id = "players";
+  table.style.top = "88px";
+  table.style.left = "450px";
+  table.style.fontFamily = "Courier New";
+  table.style.fontSize = "20px";
+  table.style.fontWeight = "bold";
+  table.style.whiteSpace = "nowrap";
+
+  var row1 = document.createElement('tr');
+  row1.style.width = width;
+  $(row1).toggleClass("blockabsolute");
+  var red = document.createElement('td');
+  var blue = document.createElement('td');
+  setTableCellStyle(red, "RED", undefined, 5);
+  setTableCellStyle(blue, "BLUE", undefined, 5);
+  red.style.top = "-25px";
+  blue.style.top = "-25px";
+  red.innerHTML = "Red team";
+  blue.innerHTML = "Blue team";
+  row1.appendChild(blue);
+  row1.appendChild(red);
+  table.appendChild(row1);
+  byId("matchHistoryPane").appendChild(table);
+}
+/*Sets the match history tab's info fields.*/
+function setTabFields() {
+  var fields = document.getElementsByClassName("infoText");
+  for (var i = 0; i < fields.length; i++)
+    fields[i].style.top = (i * (20/*font size, px*/+10/*offset between texts*/) + 50/*button(id="reverse")*/) + "px";
+
+  byId("player").innerHTML = "Player: "+settings.player+" (pid "+settings.playerId+")";
+  byId("matchMap").innerHTML = "Map: "+getMapName(match.mapId);
+  byId("matchEnv").innerHTML =
+    "Match type: "+getMatchMode(match.matchMode)+" "+getMatchType(match.matchType);
+  byId("matchQueue").innerHTML = "Match queue: "+getQueueType(match.queueType);
+  byId("matchDuration").innerHTML = "Match time: "+getHumanTime(match.matchDuration);
+  byId("matchVer").innerHTML = "Match version: "+match.matchVersion;
+  byId("matchVictor").innerHTML = "Winner: "+getWinner();
 }
--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,5 @@
 var settings = {};
+var matchId, match;
 checkSettings();
 
 function checkSettings() {
@@ -15,28 +16,25 @@ function checkSettings() {
 
   storagePromise.then(
     function(result) {
+      /*Settings present; load the data.*/
       console.log(result);
       getLastMatchId();
     },
     function(err){
+      /*Settings not present; prompt for data, get playerId, store data, load the data.*/
       console.log(err);
-      promptSettings(storeSettings);
+      promptSettings();
+      getAndStorePlayerId();
+      cacheSettings();
+      getLastMatchId();
     }
   );
 }
 
+/*Clear the storage.*/
 function clear() {
   settings = {};
   chrome.storage.local.clear();
-}
-
-function storeSettings() {
-  chrome.storage.local.set({"name": settings.name}, undefined);
-  chrome.storage.local.set({"redditUser": settings.redditUser}, undefined);
-  chrome.storage.local.set({"server": settings.server}, undefined);
-  chrome.storage.local.set({"apiKey": settings.apiKey}, undefined);
-  chrome.storage.local.set({"player": settings.player}, undefined);
-  chrome.storage.local.set({"playerId": settings.playerId}, getLastMatchId());
 }
 
 function getAndStoreSettings(onFinish) {
@@ -48,11 +46,215 @@ function getAndStoreSettings(onFinish) {
   chrome.storage.local.get("playerId", function(data){settings.playerId = data.playerId;onFinish();});
 }
 
-function promptSettings(onFinish) {
+function promptSettings() {
   settings.name = prompt("Please input a title (4 letters are recommended):");
   settings.redditUser = prompt("Please input your reddit username:");
   settings.server = prompt("Please input your League of Legends server:");
   settings.apiKey = prompt("Please input a Riot API key:");
   settings.player = prompt("Please input your League of Legends summoner name:");
-  getAndStorePlayerId(onFinish);
+}
+
+function getAndStorePlayerId() {
+  $.get(
+    "https://eune.api.pvp.net/api/lol/"+settings.server+"/v1.4/summoner/by-name/"+settings.player+"?api_key="+settings.apiKey,
+    function(data) {settings.playerId = data[settings.player.toLowerCase()].id;}
+    );
+}
+
+function cacheSettings() {
+  chrome.storage.local.set({"name": settings.name}, undefined);
+  chrome.storage.local.set({"redditUser": settings.redditUser}, undefined);
+  chrome.storage.local.set({"server": settings.server}, undefined);
+  chrome.storage.local.set({"apiKey": settings.apiKey}, undefined);
+  chrome.storage.local.set({"player": settings.player}, undefined);
+  chrome.storage.local.set({"playerId": settings.playerId}, undefined);
+}
+
+function getLastMatchId() {
+  main();
+  $.get('https://eune.api.pvp.net/api/lol/'+settings.server+'/v2.2/matchhistory/'+settings.playerId+'?api_key='+settings.apiKey,
+    function(data){
+      matchId = data.matches[data.matches.length-1].matchId;
+      getLastMatch();
+  });
+}
+
+function getLastMatch() {
+  $.get('https://eune.api.pvp.net/api/lol/'+settings.server+'/v2.2/match/'+matchId+'?api_key='+settings.apiKey,
+    function(data){
+      match = data;
+      displayMatch();
+      $(byId("LoL Data")).click(function() {
+        byId("matchHistoryPane").style.visibility = "visible";
+        moveDiv(false, "matchHistoryPane");
+        moveDiv(true, "defaultPane");
+      });
+      $(byId("Go Back")).click(function() {
+        moveDiv(true, "matchHistoryPane");
+        moveDiv(false, "defaultPane");
+      });
+  });
+}
+
+/*DATA PROCESSING -->*/
+
+function getPlayerScores() {
+  var scores = [];
+  for (var i = 0; i < match.participants.length; i++) {
+    var playerStats = match.participants[i].stats;
+    scores[scores.length] = playerStats.kills + "/" + playerStats.deaths + "/" + playerStats.assists;
+  }
+  return scores;
+}
+
+function getPlayerNames() {
+  var names = [];
+  for (var i = 0; i < match.participantIdentities.length; i++) {
+    names[names.length] = match.participantIdentities[i].player.summonerName;
+  }
+  return names;
+}
+
+function getWinner() {
+  if (match.teams[0].winner == true) return "Red";
+  else return "Blue";
+}
+
+function getHumanTime(seconds) {
+  var mins = Math.floor(seconds/60);
+  var sec = seconds-(mins*60);
+  return mins+" minutes "+sec+" seconds";
+}
+
+function getMatchMode(rawMode) {
+  switch (rawMode) {
+  case "CLASSIC":
+    return "Classic";
+    break;
+  case "ARAM":
+    return "Aram";
+    break;
+  case "ODIN":
+    return "Dominion";
+    break;
+  case "TUTORIAL":
+    return "Tutorial"
+    break;
+  default:
+    return "Featured gamemode";
+  }
+}
+
+function getMatchType(rawType) {
+  switch (rawType) {
+  case "MATCHED_GAME":
+    return "matched game";
+    break;
+  default:
+    return "custom game";
+  }
+}
+
+function getQueueType(rawQueue) {
+  switch (rawQueue) {
+  case "GROUP_FINDER_5x5":
+    return "TeamBuilder";
+    break;
+  case "NORMAL_5x5_BLIND":
+    return "Normal blind";
+    break;
+  case "NORMAL_5x5_DRAFT":
+    return "Normal draft";
+    break;
+  case "NORMAL_3x3":
+    return "Normal 3v3";
+    break;
+  case "ODIN_5x5_BLIND":
+    return "Dominion blind";
+    break;
+  case "ODIN_5x5_DRAFT":
+    return "Dominon draft (ROFL WHO PLAYS THIS)";
+    break;
+  case "RANKED_SOLO_5x5":
+    return "Ranked solo";
+    break;
+  case "RANKED_PREMADE_5x5":
+    return "Ranked duo";
+    break;
+  case "RANKED_TEAM_5x5":
+    return "Ranked team";
+    break;
+  case "RANKED_TEAM_3x3":
+    return "Ranked 3v3 team";
+    break;
+  case "ARAM_5x5":
+    return "ARAM";
+    break;
+  case "CUSTOM":
+    return "Self-created match";
+    break;
+  default:
+    return "Featured gamemode";
+  }
+}
+
+function getMapName(mapId) {
+  switch (mapId) {
+  case 11:
+    return "Summoner's Rift";
+    break;
+  case 10:
+    return "Twisted Treeline";
+    break;
+  case 12:
+    return "Howling Abyss";
+    break;
+  case 8:
+    return "The Crystal Scar";
+    break;
+  default:
+    return "Other map";
+  }
+}
+
+function getMonthName(monthnumeral) {
+  switch (monthnumeral) {
+  case 0:
+    return "January";
+    break;
+  case 1:
+    return "February";
+    break;
+  case 2:
+    return "March";
+    break;
+  case 3:
+    return "April";
+    break;
+  case 4:
+    return "May";
+    break;
+  case 5:
+    return "June";
+    break;
+  case 6:
+    return "July";
+    break;
+  case 7:
+    return "August";
+    break;
+  case 8:
+    return "September";
+    break;
+  case 9:
+    return "October";
+    break;
+  case 10:
+    return "November";
+    break;
+  case 11:
+    return "December";
+    break;
+  default: return "What did you do with this method?"
+  }
 }
