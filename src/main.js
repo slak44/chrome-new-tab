b13e9240cff72edd13c2b02c535580d429c14da7
slak44 <sylviu44@gmail.com>
2015-03-20 23:38:55 +0200

    Merged settings.js into main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
 'use strict';
+loadPlugins(executePluginsOnLoad);
+loadSettings(settingsPresent, settingsAbsent);
+
+function executeSettingsOnLoad() {
+  for (var i = 0; i < onSettingsLoad.length; i++) onSettingsLoad[i]();
+}
+
+function executePluginsOnLoad() {
+  for (var i = 0; i < plugins.length; i++) plugins[i].execute();
+}
+
+/*Settings not present; prompt for data, store data, load the data.*/
+function settingsAbsent() {
+  settings.name = prompt("Please input a title:");
+  settings.redditUser = prompt("Please input your reddit username:");
+  settingsConfig.push(function(callback) {chrome.storage.local.set({"storedSettings": settings}, callback);});
+  settingsConfig.push(executeSettingsOnLoad);
+  queue(settingsConfig, window);
+  manipulateDOM();
+}
+
+/*Settings present; load the data.*/
+function settingsPresent() {
+  setTimeout(manipulateDOM, 0);
+  executeSettingsOnLoad();
+}
+
 function manipulateDOM() {
   byId("dataPane").style.left = ($(window).width()/2) - 400 + "px";
   byId("dataPane").style.top = "0px";
