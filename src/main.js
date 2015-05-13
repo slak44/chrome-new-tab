d22ea8f7a21f3b0a6ec1f39150e7bc711c6bcfb0
slak44 <sylviu44@gmail.com>
2015-05-13 16:43:09 +0300

    Fixed main.js loading order

--- src/main.js
+++ src/main.js
@@ -13,26 +13,28 @@ var mainButtons = [
 ];
 
 storage.loadSettings(onSettings, function () {window.location.replace('/settings.html')});
-storage.loadPlugins(executePluginsOnLoad, function () {});
-
-function executePluginsOnLoad() {
-  for (var p in plugins) {
-    console.log('Executing plugin: ' + plugins[p].name);
-    eval(plugins[p].code);
-  }
-}
 
 function onSettings() {
-  for (var i = 0; i < mainButtons.length; i++) {
-    mainButtons[i].aHref.style.top = i * (75/*Button height*/ + 10/*Space between btns*/) + 'px';
-    if (mainButtons[i].name === 'Extensions')
-      mainButtons[i].aHref.addEventListener('click', function (e) {window.location.replace('chrome://extensions')});
-  }
-  byId('title').innerHTML = settings['Main page title'].value;
-  byId('date').innerHTML = new Date().toLocaleString('intl', {year:'numeric', month: 'long', day:'2-digit'});
-  setTime();
-  function setTime() {
-    byId('time').innerHTML = new Date().toLocaleTimeString().slice(0, -3);
-    setTimeout(setTime, 1000);
+  storage.loadPlugins(
+  function () {
+    for (var p in plugins) {
+      console.log('Executing plugin: ' + plugins[p].name);
+      eval(plugins[p].code);
+    }
+    setData();
+  }, setData);
+  function setData() {
+    for (var i = 0; i < mainButtons.length; i++) {
+      mainButtons[i].aHref.style.top = i * (75/*Button height*/ + 10/*Space between btns*/) + 'px';
+      if (mainButtons[i].name === 'Extensions')
+        mainButtons[i].aHref.addEventListener('click', function (e) {window.location.replace('chrome://extensions')});
+    }
+    byId('title').innerHTML = settings['Main page title'].value;
+    byId('date').innerHTML = new Date().toLocaleString('intl', {year: 'numeric', month: 'long', day: '2-digit'});
+    setTime();
+    function setTime() {
+      byId('time').innerHTML = new Date().toLocaleTimeString().slice(0, -3);
+      setTimeout(setTime, 1000);
+    }
   }
 }
