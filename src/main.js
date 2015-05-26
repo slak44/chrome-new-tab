4c067c8b8d0d5732448e7dfce0a444ae0fcbd50b
slak44 <sylviu44@gmail.com>
2015-05-26 18:33:05 +0300

    Changed page loading to be more efficient Reverted setInterval change because the first call must be immediate

--- src/main.js
+++ src/main.js
@@ -3,17 +3,25 @@ document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', '<style
 var pluginCss = byId('plugin-css');
 var identity = 'Main page';
 
-new Button('assets/gmail.png', 'https://mail.google.com/mail/?authuser=0', 'Gmail');
-new Button('assets/youtube.png', 'https://www.youtube.com/?gl=RO&authuser=0', 'Youtube');
-new Button('assets/translate.png', 'https://translate.google.com/?hl=en&authuser=0', 'Translate');
-new Button('assets/reddit.png', 'https://www.reddit.com', 'Reddit');
-new Button('assets/github.png', 'https://github.com/', 'GitHub');
-new Button('assets/twitch.png', 'http://www.twitch.tv', 'Twitch');
-new Button('assets/extensions.png', 'chrome://extensions', 'Extensions');
+setTimeout(function () {
+  new Button('assets/gmail.png', 'https://mail.google.com/mail/?authuser=0', 'Gmail');
+  new Button('assets/youtube.png', 'https://www.youtube.com/?gl=RO&authuser=0', 'Youtube');
+  new Button('assets/translate.png', 'https://translate.google.com/?hl=en&authuser=0', 'Translate');
+  new Button('assets/reddit.png', 'https://www.reddit.com', 'Reddit');
+  new Button('assets/github.png', 'https://github.com/', 'GitHub');
+  new Button('assets/twitch.png', 'http://www.twitch.tv', 'Twitch');
+  new Button('assets/extensions.png', 'chrome://extensions', 'Extensions');
+  byId('date').innerHTML = new Date().toLocaleDateString('en-GB', {month: 'long', day: '2-digit', year: 'numeric'});
+}, 0);
+setTimeout(function setTime() {
+  byId('time').innerHTML = new Date().toLocaleTimeString('intl', {hour: '2-digit', minute: '2-digit', hour12: false});
+  setTimeout(setTime, 1000);
+}, 0);
 
 storage.loadSettings(onSettings, function () {window.location.replace('/settings.html')});
 
 function onSettings() {
+  byId('title').innerHTML = settings['Main page title'].value;
   storage.loadPlugins(
   function () {
     for (var p in plugins) {
@@ -21,9 +29,4 @@ function onSettings() {
       eval(plugins[p].code);
     }
   }, function () {console.log('No plugins executed.')});
-  byId('title').innerHTML = settings['Main page title'].value;
-  byId('date').innerHTML = new Date().toLocaleDateString('en-GB', {month: 'long', day: '2-digit', year: 'numeric'});
-  setInterval(function () {
-    byId('time').innerHTML = new Date().toLocaleTimeString('intl', {hour: '2-digit', minute: '2-digit', hour12: false});
-  }, 1000);
 }
