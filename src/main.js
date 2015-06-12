66ef65df3dcc2f4fd8283fb1fea67038e34f4df1
slak44 <sylviu44@gmail.com>
2015-06-12 18:18:12 +0300

    Removed old buttons

--- src/main.js
+++ src/main.js
@@ -17,13 +17,6 @@ setTimeout(function () {
     new Button(undefined, '/settings.html', 'Configure buttons here');
   });
   byId('date').innerHTML = new Date().toLocaleDateString('en-GB', {month: 'long', day: '2-digit', year: 'numeric'});
-  // new Button('assets/gmail.png', 'https://mail.google.com/mail/?authuser=0', 'Gmail');
-  // new Button('assets/youtube.png', 'https://www.youtube.com/?gl=RO&authuser=0', 'Youtube');
-  // new Button('assets/translate.png', 'https://translate.google.com/?hl=en&authuser=0', 'Translate');
-  // new Button('assets/reddit.png', 'https://www.reddit.com', 'Reddit');
-  // new Button('assets/github.png', 'https://github.com/', 'GitHub');
-  // new Button('assets/twitch.png', 'http://www.twitch.tv', 'Twitch');
-  // new Button('assets/extensions.png', 'chrome://extensions', 'Extensions');
 }, 0);
 setTimeout(function setTime() {
   byId('time').innerHTML = new Date().toLocaleTimeString('intl', {hour: '2-digit', minute: '2-digit', hour12: false});
