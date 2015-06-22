52c0afa4e25cd8353ac32061386ef238f6f4ac05
slak44 <sylviu44@gmail.com>
2015-06-22 19:29:56 +0300

    Fixed race condition between buttons and plugins Now using async.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', '<style
 var pluginCss = byId('plugin-css');
 var identity = 'Main page';
 
-setTimeout(function () {
+setTimeout(function setTime() {
+  byId('time').innerHTML = new Date().toLocaleTimeString('intl', {hour: '2-digit', minute: '2-digit', hour12: false});
+  setTimeout(setTime, 1000);
+}, 0);
+
+async.parallel([loadButtons, loadSettings],
+  loadPlugins);
+
+function loadButtons(cb) {
   storage.load('buttons',
   function () {
     var orderedButtons = [];
@@ -13,19 +21,19 @@ setTimeout(function () {
       else return 1;
     });
     for (var i = 0; i < orderedButtons.length; i++) new Button(orderedButtons[i].imagePath, orderedButtons[i].href, orderedButtons[i].text);
+    cb();
   }, function () {
     new Button(undefined, '/secondary.html', 'Configure buttons here');
+    cb();
   });
   byId('date').innerHTML = new Date().toLocaleDateString('en-GB', {month: 'long', day: '2-digit', year: 'numeric'});
-}, 0);
-setTimeout(function setTime() {
-  byId('time').innerHTML = new Date().toLocaleTimeString('intl', {hour: '2-digit', minute: '2-digit', hour12: false});
-  setTimeout(setTime, 1000);
-}, 0);
+}
 
-storage.load('settings', onSettings, function () {window.location.replace('/secondary.html')});
+function loadSettings(cb) {
+  storage.load('settings', cb, function () {window.location.replace('/secondary.html')});
+}
 
-function onSettings() {
+function loadPlugins() {
   // If a visible value is empty, it fails immediately
   for (var e in settings) if (settings[e].isVisible && settings[e].value === undefined) window.location.replace('/secondary.html');
   byId('title').innerHTML = settings['Main page title'].value;
