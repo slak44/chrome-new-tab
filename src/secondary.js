124961ce44a3e5124d19c0567458c8e6c99c2ee4
Slak44 <sylviu44@gmail.com>
2015-12-08 16:39:23 +0200

    Replaced 'var' with 'let'

--- src/secondary.js
+++ src/secondary.js
@@ -1,5 +1,5 @@
 'use strict';
-var activeSchemeIndex = 0;
+let activeSchemeIndex = 0;
 
 async.parallel([loadButtons, loadPlugins, loadSchemesAndUI], function (err) {
   if (err) throw err;
@@ -7,15 +7,15 @@ async.parallel([loadButtons, loadPlugins, loadSchemesAndUI], function (err) {
 
 byId('floating-save-button').addEventListener('click', function (evt) {
   if (hasClass(byId('settings-tab'), 'focused')) {
-    var currentPlugin = byQSelect('.plugin-container.focused');
-    var cpId = currentPlugin.id.slice(0, -10);
+    let currentPlugin = byQSelect('.plugin-container.focused');
+    let cpId = currentPlugin.id.slice(0, -10);
     Array.prototype.forEach.apply(currentPlugin.children, [function (settingDiv, i, children) {
       if (i <= 1) return; // Ignore the title and the description
       plugins[cpId].settings[i - 2].value = settingDiv.children[0].value;
     }]);
 		storage.store('plugins');
 	} else if (hasClass(byId('buttons-tab'), 'focused')) {
-		var id = byId('buttonText').getAttribute('data-button-id');
+		let id = byId('buttonText').getAttribute('data-button-id');
 		if (id === '') return;
 		buttons[id] = {
 			text: byId('buttonText').value,
@@ -28,7 +28,7 @@ byId('floating-save-button').addEventListener('click', function (evt) {
 		storage.store('buttons');
 	} else if (hasClass(byId('json-tab'), 'focused')) {
 	  if (byId('insert-data').value !== '') {
-	    var data = JSON.parse(byId('insert-data').value);
+	    let data = JSON.parse(byId('insert-data').value);
 	    if (data.pluginsData) {
         plugins = data.pluginsData;
         storage.store('plugins');
@@ -40,7 +40,7 @@ byId('floating-save-button').addEventListener('click', function (evt) {
 	  }
 	} else if (hasClass(byId('color-scheme-tab'), 'focused')) {
 		// Switch the active one at the top
-		var originalScheme = colorScheme[0];
+		let originalScheme = colorScheme[0];
 		colorScheme[0] = colorScheme[activeSchemeIndex];
 		colorScheme[activeSchemeIndex] = originalScheme;
 		storage.store('colorScheme');
@@ -65,7 +65,7 @@ byId('copy-data').addEventListener('click', function (event) {
     buttonsData: buttons
   });
 	byId('temp-data').select();
-  var status = document.execCommand('copy');
+  let status = document.execCommand('copy');
 	console.log(status);
 });
 
@@ -84,7 +84,7 @@ byId('add-scheme').addEventListener('click', function (e) {
 byId('remove-scheme').addEventListener('click', function (e) {
 	if (!confirm('Remove this scheme?')) return;
   colorScheme.splice(activeSchemeIndex, 1);
-	var schemeElement = byQSelect('#color-scheme-list > a.active');
+	let schemeElement = byQSelect('#color-scheme-list > a.active');
 	schemeElement.parentNode.removeChild(schemeElement);
 	byId('color-scheme-list').children[0].classList.add('active');
 	storage.store('colorScheme');
@@ -92,7 +92,7 @@ byId('remove-scheme').addEventListener('click', function (e) {
 
 byId('add-buttons').addEventListener('click', function (e) {
   e.preventDefault();
-  var id = prompt('Input a unique identifier for the button:');
+  let id = prompt('Input a unique identifier for the button:');
   if (id === null) return;
   if (buttons[id] !== undefined && buttons[id] !== null) {
     alert('Identifier already exists.');
@@ -112,7 +112,7 @@ byId('add-buttons').addEventListener('click', function (e) {
 byId('remove-buttons').addEventListener('click', function (e) {
   e.preventDefault();
   if (!confirm('Are you sure you want to delete this button?')) return;
-  var id = byId('buttonText').getAttribute('data-button-id');
+  let id = byId('buttonText').getAttribute('data-button-id');
   delete buttons[id];
   byId(id).parentNode.removeChild(byId(id));
   setCurrentButton(getFirstButton());
@@ -127,8 +127,8 @@ function loadButtons(cb) {
       return;
     }
 		addButtonConfig(Object.keys(buttons)[0]);
-    var dropdown = byId('buttons-list');
-    for (var id in buttons) {
+    let dropdown = byId('buttons-list');
+    for (let id in buttons) {
 			dropdown.insertAdjacentHTML('beforeend', '<li id="' + id + '"><a href="#!">' + id + '</a></li>');
 			byId(id).addEventListener('click', (function (id) {
 				return function (evt) {
@@ -167,7 +167,7 @@ function getFirstButton() {
 function loadPlugins(cb) {
   storage.load('plugins', function (error) {
     if (!error) Object.keys(plugins).forEach(function (name, i, array) {
-      var plugin = plugins[name];
+      let plugin = plugins[name];
       console.log('Executing plugin: ' + plugin.name);
       addPluginData(plugin, !byClass('plugin-container').length);
       /*jshint -W061 */
@@ -184,7 +184,7 @@ function loadPlugins(cb) {
 function addPluginData(plugin, focus) {
   byId('plugins-list').insertAdjacentHTML('beforeend', '<li id="' + plugin.name + '"><a href="#!">' + plugin.name + '</a></li>');
   byId('settings-tab').insertAdjacentHTML('beforeend', '<div id="' + plugin.name + '-container" class="plugin-container ' + (focus ? 'focused' : 'unfocused') + '"></div>');
-  var container = byId(plugin.name + '-container');
+  let container = byId(plugin.name + '-container');
   container.insertAdjacentHTML('beforeend', '<h5 class="plugin-title">' + plugin.name + '</h5><p class="plugin-desc">' + plugin.desc + '<br>' + plugin.version + ' by ' + plugin.author + '</p>');
   if (plugin.settings) {
     plugin.settings.forEach(function (setting, i, settings) {
@@ -200,7 +200,7 @@ function addPluginData(plugin, focus) {
     container.insertAdjacentHTML('beforeend', '<p>There isn\'t anything here</p>');
   }
   byId(plugin.name).addEventListener('click', function (evt) {
-    var container = byQSelect('.plugin-container.focused');
+    let container = byQSelect('.plugin-container.focused');
     toggleDiv(container, true);
     container.style.display = 'none';
     byId(plugin.name + '-container').style.display = 'block';
@@ -212,7 +212,7 @@ function loadSchemesAndUI() {
   loadSchemes(function () {
 		activateScheme(colorScheme[0]);
 		colorScheme.forEach(function (scheme, i, array) {
-			var htmlContent = '<a href="#!" class="collection-item color">' + scheme.name + '<div class="row top-margin">';
+			let htmlContent = '<a href="#!" class="collection-item color">' + scheme.name + '<div class="row top-margin">';
 			Object.keys(scheme).forEach(function (color, i, array) {
 			  if (color === 'name') return;
 				if (color === 'isDark') {
@@ -226,7 +226,7 @@ function loadSchemesAndUI() {
 			Array.prototype.forEach.apply(byId('color-scheme-list').children, [function (schemeElement, i, arr) {
 				if (i === 0) schemeElement.classList.add('active');
 			  schemeElement.addEventListener('click', function (evt) {
-					var actives = byQSelect('#color-scheme-list > a.active');
+					let actives = byQSelect('#color-scheme-list > a.active');
 					if (actives) actives.classList.remove('active');
 			    schemeElement.classList.add('active');
 					activeSchemeIndex = i;
@@ -266,8 +266,8 @@ function addButtonConfig(buttonId) {
 }
 
 function addPlugins(event, allowUpdate) {
-  var file = event.target.files[0];
-  var reader = new FileReader();
+  let file = event.target.files[0];
+  let reader = new FileReader();
   reader.onloadend = (function (fileIn) {
     return function (e) {
       if (fileIn.type !== "application/javascript") {
@@ -275,7 +275,7 @@ function addPlugins(event, allowUpdate) {
         return;
       }
       /*jshint -W061*/
-      var plugin = eval(e.target.result);
+      let plugin = eval(e.target.result);
       if (plugin.init) plugin.init();
       // Serialize functions, if they exist
       if (typeof plugin.init === 'function') plugin.init = plugin.init.toString();
