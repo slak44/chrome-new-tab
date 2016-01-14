e4f14be2fc14227ab266c94421945c515c2935e8
Slak44 <sylviu44@gmail.com>
2016-01-14 20:10:18 +0200

    Updated plugins for new panel system

--- plugins/title.js
+++ plugins/title.js
@@ -4,15 +4,30 @@ var plugin = {
   desc: 'Adds a title to the main page',
   author: 'Slak44',
   version: '1.0.0',
-  settings: [{
-    name: 'Title text',
-    desc: 'The text to be displayed',
-    type: 'text',
-    value: '',
-    isVisible: true
-  }],
+  settings: [
+    {
+      name: 'Title text',
+      desc: 'The text to be displayed',
+      type: 'text',
+      value: '',
+      isVisible: true
+    },
+    {
+      name: 'Position',
+      desc: 'The text will be inserted after this many elements',
+      type: 'number',
+      value: '',
+      isVisible: true
+    }
+  ],
   main: function (plugin) {
-    byId('data-collection').insertAdjacentHTML('afterbegin', '<li id="title" class="collection-item bold"><h1>' + plugin.settings[0].value + '</h1></li>');
+    addPanel({
+      position: plugin.settings[1] || 0,
+      htmlContent:
+      `<li id="title" class="collection-item bold">
+        <h1>${plugin.settings[0].value}</h1>
+      </li>`
+    });
   }
 };
 /* jshint -W030*/
