554ce255863c0cc83bec6c582c22cdf97bbb060f
Slak44 <sylviu44@gmail.com>
2016-01-14 18:51:59 +0200

    Updated reddit plugin

--- plugins/reddit.js
+++ plugins/reddit.js
@@ -1,9 +1,9 @@
 'use strict';
-var plugin = {
+let plugin = {
   name: 'Reddit Karma Info',
   desc: 'Displays reddit karma for given user',
   author: 'Slak44',
-  version: '1.1.1',
+  version: '1.2.0',
   settings: [
     {
       name: 'Reddit username',
@@ -23,23 +23,26 @@ var plugin = {
   init: function () {
   },
   main: function (plugin) {
-    var url = 'https://www.reddit.com/user/' + plugin.settings[0].value;
+    let url = `https://www.reddit.com/user/${plugin.settings[0].value}`;
     function updateRedditKarma() {
-      var req = new XMLHttpRequest();
+      let req = new XMLHttpRequest();
       req.open('GET', url + '/about.json');
       req.onload = function () {
         if (req.status === 200) {
-          var data = JSON.parse(req.response);
+          let data = JSON.parse(req.response);
           byId('reddit-karma').innerHTML =
-            '<a href="'+url+'" class="color accent-4">' + data.data.comment_karma + '</a> comment karma<br><a href="'+url+'" class="color accent-4">' + data.data.link_karma + '</a> link karma';
+            `<a href="${url}" class="color accent-4">${data.data.comment_karma}</a> comment karma
+            <br>
+            <a href="${url}" class="color accent-4">${data.data.link_karma}</a> link karma`;
         } else throw new Error('Failed request.');
       };
-      req.onloadend = function () {
-        setTimeout(updateRedditKarma, plugin.settings[1].value);
-      };
+      req.onloadend = () => setTimeout(updateRedditKarma, plugin.settings[1].value);
       req.send();
     }
-    byId('data-collection').insertAdjacentHTML('beforeend', '<li class="collection-item"><h5 id="reddit-karma"></h5></li>');
+    byId('data-collection').insertAdjacentHTML('beforeend',
+    `<li class="collection-item">
+      <h5 id="reddit-karma"></h5>
+    </li>`);
     setTimeout(updateRedditKarma, 0);
   }
 };
