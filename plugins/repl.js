51fa4ab4bc7110250416b98fcb0af7a82e8ce0c5
Slak44 <sylviu44@gmail.com>
2016-03-01 21:00:07 +0200

    Greatly improved REPL commands

--- plugins/repl.js
+++ plugins/repl.js
@@ -1,5 +1,30 @@
 'use strict';
 function run() {
+  setTimeout(function () {
+    // Load currency conversion script & get currency rates
+    let money = new XMLHttpRequest();
+    money.onload = function () {
+      eval.apply(window, [money.responseText]);
+      // Get current exchange rates
+      fx.base = 'EUR';
+      let rates = new XMLHttpRequest();
+      rates.onload = () => fx.rates = JSON.parse(rates.responseText).rates;
+      rates.open('GET', 'https://api.fixer.io/latest');
+      rates.send();
+    };
+    money.open('GET', 'https://raw.githubusercontent.com/openexchangerates/money.js/master/money.min.js');
+    money.send();
+  }, 0);
+  setTimeout(function () {
+    // Load quantity conversion script
+    let quantityJs = new XMLHttpRequest();
+    quantityJs.onload = function () {
+      eval.apply(window, [quantityJs.responseText]);
+    };
+    quantityJs.open('GET', 'https://raw.githubusercontent.com/gentooboontoo/js-quantities/v1.6.0/src/quantities.js');
+    quantityJs.send();
+  }, 0);
+
   pluginCss.innerHTML += `
   #repl-pane {
     opacity: 0;
@@ -65,11 +90,49 @@ function run() {
     </div>
   </div>
   `);
+  function openInNewTab(url) {
+    let anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.target = '_blank';
+    anchor.click();
+    return anchor.href;
+  }
+  // Syntax: `!COMMAND_NAME ARGS`
+  // ARGS will be passed as a string to each command
+  let commands = {
+    convert: function (data) {
+      // !convert 100 usd to EUR
+      // !convert 50 m to km
+      data = data.split(' ');
+      if (data[2].toLowerCase() === 'to') data.splice(2, 1);
+      data[1] = data[1].toUpperCase();
+      data[2] = data[2].toUpperCase();
+      // If the unit is currency, use the currency script
+      if (Object.keys(fx.rates).includes(data[1])) {
+        return fx.convert(Number(data[0]), {from: data[1], to: data[2]}).toFixed(2) + ` ${data[2]}`;
+      }
+      return Qty(`${data[0]} ${data[1]}`).to(data[2]).toString();
+    },
+    query: function (data) {
+      return openInNewTab(`https://www.google.ro/search?q=${encodeURIComponent(data)}`);
+    },
+    wolfram: function (data) {
+      return openInNewTab(`http://www.wolframalpha.com/input/?i=${encodeURIComponent(data)}`);
+    }
+  };
+  let commandAliases = {
+    cv: 'convert',
+    q: 'query',
+    w: 'wolfram'
+  };
+  
   const keycodes = {
     enter: 13,
     upArrow: 38,
     downArrow: 40
   };
+  Object.freeze(keycodes);
+  
   let result;
   let commandHistory = [];
   let rewindCount = 0;
@@ -86,16 +149,19 @@ function run() {
     if (event.keyCode === keycodes.enter) {
       rewindCount = 0;
       let oldElem = byClass('current-text')[0];
-      let code = oldElem.textContent.replace(/console\.(log|error|info|debug)/g, 'replLog'); // Replace calls to 'console'
-      if (code.startsWith('convert ')) {
-        result = convert(code.replace('convert ', ''));
-      } else {
-        /*jshint -W061 */
-        try {
+      let code = oldElem.textContent
+        .replace(/console\.(log|error|info|debug)/g, 'replLog') // Replace calls to 'console'
+        .trim(); // Remove leading/trailing whitespace
+      try {
+        if (code.startsWith('!')) {
+          let commandName = code.substr(1, code.indexOf(' ') - 1);
+          result = (commands[commandName] || commands[commandAliases[commandName]])(code.replace(`!${commandName} `, ''));
+        } else {
+          /*jshint -W061 */
           result = eval(code);
-        } catch (error) {
-          result = error;
         }
+      } catch (e) {
+        result = error;
       }
       commandHistory.push(code);
       oldElem.classList.remove('current-text');
@@ -137,31 +203,12 @@ function run() {
   }
   repl.addEventListener('click', toggle);
   byId('repl-back').addEventListener('click', toggle);
-  // Load currency conversion script & get currency rates
-  let money = new XMLHttpRequest();
-  money.onload = function () {
-    eval.apply(window, [money.responseText]);
-    // Syntactic sugar. Usage: convert('123 USD EUR') or convert('123 usd to eur')
-    window.convert = function (data) {
-      data = data.split(' ');
-      if (data[2].toLowerCase() === 'to') data.splice(2, 1);
-      return fx.convert(Number(data[0]), {from: data[1].toUpperCase(), to: data[2].toUpperCase()}).toFixed(2);
-    };
-    // Get current exchange rates
-    fx.base = 'EUR';
-    let rates = new XMLHttpRequest();
-    rates.onload = () => fx.rates = JSON.parse(rates.responseText).rates;
-    rates.open('GET', 'https://api.fixer.io/latest');
-    rates.send();
-  };
-  money.open('GET', 'https://raw.githubusercontent.com/openexchangerates/money.js/master/money.min.js');
-  money.send();
 }
 let plugin = {
   name: 'REPL',
   desc: 'Read-Eval-Print-Loop',
   author: 'Slak44',
-  version: '2.0.6',
+  version: '2.1.6',
   main: run
 };
 /*jshint -W030 */
