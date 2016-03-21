80977dd5bf494c3d63b5e9d19f28b2f09a9ab4e8
Slak44 <sylviu44@gmail.com>
2016-03-21 18:44:24 +0200

    Refactored lol plugin to use async.waterfall

--- plugins/lol.js
+++ plugins/lol.js
@@ -36,13 +36,8 @@ var plugin = {
     }
   ],
   main: function (plugin) {
-    const DEBUG = true;
+    const DEBUG = false;
     const TEST_DATA_PATH = '';
-    let requests = {
-      playerByName: null,
-      currentGame: null,
-      players: []
-    };
     pluginCss.innerHTML += `
     #ingame-pane {
       opacity: 0;
@@ -69,57 +64,120 @@ var plugin = {
         if (!granted) alert('Cannot proceed without origin permissons.');
       });
     });
-    setTimeout(function () {
-      let eventemitter2 = new XMLHttpRequest();
-      eventemitter2.onload = function () {
-        eval.apply(window, [eventemitter2.responseText]);
-        getApiUtil();
-      };
-      eventemitter2.open('GET', 'https://raw.githubusercontent.com/asyncly/EventEmitter2/1.0.0/lib/eventemitter2.js');
-      eventemitter2.send();
-    }, 0);
-    function getApiUtil() {
-      let apiUtil = new XMLHttpRequest();
-      apiUtil.onload = function () {
-        window.ApiCaller = eval.apply(window, [apiUtil.responseText]);
-        checkPlayer(new window.ApiCaller(
-          [
-            {"requests": 10, "time": 10, "unit": "second"},
-            {"requests": 500, "time": 10, "unit": "minutes"}
-          ],
-          plugin.settings[2].value));
-      };
-      apiUtil.open('GET', 'https://gist.githubusercontent.com/slak44/45821e7257b72e27fbbe/raw/browserApiUtil.js');
-      apiUtil.send();
-    }
-    function checkPlayer(apiCaller) {
-      if (DEBUG) {
-        getDebugInfo(apiCaller);
-        addInfoPanel();
-        return;
-      }
-      apiCaller.getPlayerData(plugin.settings[1].value, plugin.settings[0].value, function (err, data) {
+    let requests = {
+      playerByName: null,
+      currentGame: null,
+      playerSummaries: [],
+      playerLeagues: null
+    };
+    if (DEBUG) {
+      async.waterfall([
+        getEventEmitter,
+        getApiUtil,
+        getDebugInfo,
+        addDataToPane
+      ], function (err) {
         if (err) throw err;
-        apiCaller.getCurrentGame(plugin.settings[1].value, data.id, function (err, cgData) {
-          if (err) {
-            if (err.toString().includes('404')) console.info('Player not ingame.');
-            else throw err;
-            return;
-          }
-          addInfoPanel();
-        });
+      });
+    } else {
+      async.waterfall([
+        getEventEmitter,
+        getApiUtil,
+        getPlayerData,
+        getCurrentGame,
+        getParticipantSummaries,
+        getParticipantLeagues,
+        addDataToPane
+      ], function (err) {
+        if (err) throw err;
       });
     }
-    function getDebugInfo(apiCaller) {
+    
+    function getDebugInfo(apiCaller, callback) {
+      addInfoPanel();
       let testData = new XMLHttpRequest();
-      testData.onloadend = function () {
-        let data = JSON.parse(this.responseText);
+      testData.addEventListener('loadend', function () {
+        let data = JSON.parse(testData.responseText);
         requests.playerByName = data['summoner-by-name'];
         requests.currentGame = data['current-game'];
-      };
+        requests.playerSummaries = data['player-summaries'];
+        callback(null, apiCaller);
+      });
       testData.open('GET', TEST_DATA_PATH);
       testData.send();
     }
+    
+    function getEventEmitter(callback) {
+      let eventemitter2 = new XMLHttpRequest();
+      eventemitter2.addEventListener('loadend', function () {
+        if (eventemitter2.status === 200) {
+          eval.apply(window, [eventemitter2.responseText]);
+          callback(null);
+        } else {
+          callback(new Error(`Status code ${eventemitter2.status}`));
+        }
+      });
+      eventemitter2.open('GET', 'https://raw.githubusercontent.com/asyncly/EventEmitter2/1.0.0/lib/eventemitter2.js');
+      eventemitter2.send();
+    }
+    
+    function getApiUtil(callback) {
+      let apiUtil = new XMLHttpRequest();
+      apiUtil.addEventListener('loadend', function () {
+        window.ApiCaller = eval.apply(window, [apiUtil.responseText]);
+        if (apiUtil.status === 200) {
+          eval.apply(window, [apiUtil.responseText]);
+          let apiCaller = new window.ApiCaller(
+            [
+              {requests: 10, time: 10, unit: 'seconds'},
+              {requests: 500, time: 10, unit: 'minutes'}
+            ],
+            plugin.settings[2].value);
+          callback(null, apiCaller);
+        } else {
+          callback(new Error(`Status code ${apiUtil.status}`));
+        }
+      });
+      apiUtil.open('GET', 'https://gist.githubusercontent.com/slak44/45821e7257b72e27fbbe/raw/browserApiUtil.js');
+      apiUtil.send();
+    }
+    
+    function getPlayerData(apiCaller, callback) {
+      apiCaller.getPlayerData(plugin.settings[1].value, plugin.settings[0].value, function (err, data) {
+        if (err) {
+          callback(err);
+          return;
+        }
+        requests.playerByName = data;
+        callback(null, apiCaller);
+      });
+    }
+    
+    function getCurrentGame(apiCaller, callback) {
+      apiCaller.getCurrentGame(plugin.settings[1].value, requests.playerByName.id, function (err, data) {
+        if (err) {
+          if (err.toString().includes('404')) callback(new Error('Player not ingame'));
+          else callback(err);
+          return;
+        }
+        requests.currentGame = data;
+        addInfoPanel();
+        callback(null, apiCaller);
+      });
+    }
+    
+    function getParticipantSummaries(apiCaller, callback) {
+      // TODO
+    }
+    
+    function getParticipantLeagues(apiCaller, callback) {
+    // TODO  
+    }
+    
+    function addDataToPane(apiCaller, callback) {
+    // TODO  
+    }
+    
     function addInfoPanel() {
       addPanel({
         position: plugin.settings[3].value || 0,
