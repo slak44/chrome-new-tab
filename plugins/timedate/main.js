15976e9f10e15dbd8fb96bc1786b7a24c37af8c7
Slak44 <sylviu44@gmail.com>
2016-05-07 19:20:40 +0300

    Added locale settings to time/date plugin

--- plugins/timedate/main.js
+++ plugins/timedate/main.js
@@ -1,5 +1,11 @@
 'use strict';
 const timeUpdateInterval = 1000;
+const locales = {
+  time: plugins[pluginName].settings[1].value || 'intl',
+  date: plugins[pluginName].settings[2].value || 'intl',
+  weekday: plugins[pluginName].settings[3].value || 'intl'
+};
+
 addPanel({
   position: plugins[pluginName].settings[0].value || 0,
   htmlContent:
@@ -7,13 +13,13 @@ addPanel({
     <h5>
       <span id="time" class="right">00:00</span>
       <br>
-      <span id="weekday" class="right">${new Date().toLocaleDateString('en-GB', {weekday: 'long'})}</span>
+      <span id="weekday" class="right">${new Date().toLocaleDateString(locales.weekday, {weekday: 'long'})}</span>
       <br>
-      <span id="date">${new Date().toLocaleDateString('en-GB', {month: 'long', day: '2-digit', year: 'numeric'})}</span>
+      <span id="date">${new Date().toLocaleDateString(locales.date, {month: 'long', day: '2-digit', year: 'numeric'})}</span>
     </h5>
   </li>`
 });
 setTimeout(function setTime() {
-  byId('time').innerHTML = new Date().toLocaleTimeString('intl', {hour: '2-digit', minute: '2-digit', hour12: false});
+  byId('time').innerHTML = new Date().toLocaleTimeString(locales.time, {hour: '2-digit', minute: '2-digit', hour12: false});
   setTimeout(setTime, timeUpdateInterval);
 }, 0);
