f508adf3aeda58ca6d6f3bfb986861f80ec090b4
Stefan Silviu <sylviu44@gmail.com>
2018-01-06 10:54:39 +0200

    Remove old code, part 13

--- src/main/main.js
+++ src/main/main.js
@@ -4,11 +4,6 @@ require('global');
 const async = require('async');
 const buttonsUtil = require('buttons');
 
-// loadSchemes(() => {
-//   activateScheme(colorSchemes[0]);
-//
-// });
-
 async.parallel([loadButtons, loadPlugins], (err, results) => {
   if (err) throw err;
   plugins.forEach(plugin => runViewContent(plugin, 'main'));
@@ -35,36 +30,6 @@ document.onkeydown = function (e) {
   if (e.altKey) window.location.replace(buttons.find(button => button.hotkey.charCodeAt() === e.keyCode).href);
 };
 
-// function createButton(options) {
-//   if (options.parent && !(options.parent instanceof HTMLElement)) throw new Error('options.parent must be a HTMLElement');
-//   const parent = options.parent || byId('buttons');
-//   switch (options.kind) {
-//     case 'divider': parent.insertAdjacentHTML('beforeend', '<li><div class="divider"></div></li>'); return;
-//     case 'subheader': parent.insertAdjacentHTML('beforeend', `<li><a class="subheader">${options.text}</a></li>`); return;
-//     default: break;
-//   }
-//   let picture = '';
-//   switch (options.pictureType) {
-//     case 'image': if (options.imagePath) picture = `<img src="${options.imagePath}" class="button-image"/>`; break;
-//     case 'icon': if (options.ligatureName) picture = `<i class="material-icons">${options.ligatureName}</i>`; break;
-//     default: picture = '';
-//   }
-//   parent.insertAdjacentHTML('beforeend', `
-//   <li class="waves-effect">
-// 		<a href="${options.href || ''}">
-//       ${picture}
-//       <span class="button-content">${options.text}</span>
-// 		</a>
-// 	</li>`);
-//   const anchor = parent.children[parent.children.length - 1];
-//   if (options.href !== undefined && (options.href.indexOf('chrome://') === 0 || options.openInNew))
-//     anchor.addEventListener('click', event => {
-//       chrome.tabs.create({url: options.href});
-//       window.close();
-//     });
-//   return anchor;
-// }
-
 function loadButtons(callback) {
   storage.load('buttons', err => {
     if (err) {
@@ -73,19 +38,6 @@ function loadButtons(callback) {
       // createButton({text: 'Configure buttons here', href: '/secondary/secondary.html'});
     } else {
       buttonsUtil.sorted().forEach(button => buttonsUtil.insertButton(button, $('#buttons')[0]));
-      // const orderedButtons = [];
-      // for (const i in buttons) orderedButtons.push(buttons[i]);
-      // orderedButtons.sort((a, b) => {
-      //   const [x, y] = [Number(a.position), Number(b.position)];
-      //   if (x < y) return -1;
-      //   else if (x > y) return 1;
-      //   if (a.kind === 'divider') return -1;
-      //   if (b.kind === 'divider') return 1;
-      //   if (a.kind === 'subheader') return -1;
-      //   if (b.kind === 'subheader') return 1;
-      //   return (a.text < b.text) ? -1 : 1;
-      // });
-      // orderedButtons.forEach(e => createButton(e));
     }
     callback(null);
   });
--- src/secondary/secondary.js
+++ src/secondary/secondary.js
@@ -18,11 +18,6 @@ $(window).on('beforeunload', () => {
 
 byId('version-string').innerText = `version ${chrome.runtime.getManifest().version}`;
 
-// loadSchemes(() => {
-//   activateScheme(colorSchemes[0]);
-//   colorSchemes.forEach(scheme => schemesUtil.insertPreviewHTML(scheme));
-//   schemesUtil.initSchemesEventListeners();
-// });
 $(document).ready(() => {
   themes.forEach(themesUtil.addThemeSettingsUI);
   themesUtil.setUpInitialUI();
@@ -121,37 +116,22 @@ function updateButtonPreview() {
 function loadButtons(callback) {
   storage.load('buttons', error => {
     if (error) {
-      // buttons = {};
+      // FIXME
       callback(error);
       return;
     }
-    // if (Object.keys(buttons).length === 0) {
-    //   callback(null);
-    //   return;
-    // }
     buttons.forEach(buttonsUtil.addSettingCard);
     updateButtonPreview();
     $('#buttons-container input').on('change keyup paste', event => updateButtonPreview());
-    // buttonsUtil.activateDefaultButton();
-    // buttonsUtil.initDropdown();
     callback(null);
   });
 }
 
+// FIXME
 function runPlugins() {
   plugins.forEach((plugin, idx) => {
     pluginsUtil.appendPluginUI(plugin, idx);
     runViewContent(plugin, 'secondary');
-    // pluginsUtil.insertPluginSettingsHTML(plugin, !byClass('plugin-container').length); // Only the first addition gets focus
-    // try {
-    //   if (plugin.html.secondary) Object.keys(plugin.html.secondary).forEach((selector, i, array) => {
-    //     byQSelect(selector).insertAdjacentHTML('beforeend', array[selector]);
-    //   });
-    //   if (plugin.css.secondary) pluginCss.innerHTML += plugin.css.secondary;
-    //   if (plugin.js.secondary) eval(plugin.js.secondary);
-    // } catch (err) {
-    //   console.error(`Execution for ${plugin.name} failed: `, err);
-    // }
   });
 }
 
@@ -169,93 +149,4 @@ byId('floating-save-button').addEventListener('click', event => {
   storage.store('themes');
   storage.store('currentThemeIdx');
   window.changesMade = false;
-  // if (hasClass(byId('settings-tab'), 'focused')) {
-  //   pluginsUtil.saveFocusedPluginSettings();
-  // } else if (hasClass(byId('buttons-tab'), 'focused')) {
-  //   const id = byId('button-text').getAttribute('data-button-id');
-  //   if (id === '') return;
-  //   buttonsUtil.saveFocusedButton(id);
-  // } else if (hasClass(byId('color-scheme-tab'), 'focused')) {
-  //   schemesUtil.saveSelected();
-  // }
 });
-
-// function showTab(id) {
-//   return function (event) {
-//     event.preventDefault();
-//     toggleDiv(byQSelect('.data-tab.focused'));
-//     toggleDiv(id);
-//   };
-// }
-// byId('plugin-settings').addEventListener('click', showTab('settings-tab'));
-// byId('button-list').addEventListener('click', showTab('buttons-tab'));
-// byId('backup-and-restore').addEventListener('click', showTab('json-tab'));
-// byId('color-scheme').addEventListener('click', showTab('color-scheme-tab'));
-//
-// function createDataJson() {
-//   byId('copy-data-display').value = JSON.stringify({
-//     pluginsData: plugins,
-//     buttonsData: buttons
-//   });
-// }
-// byId('show-data').addEventListener('click', event => {
-//   createDataJson();
-// });
-//
-// byId('copy-data').addEventListener('click', event => {
-//   createDataJson();
-//   byId('copy-data-display').select();
-//   document.execCommand('copy');
-//   byId('copy-data-display').value = '';
-//   byId('copy-data').focus();
-// });
-//
-// byId('restore-data').addEventListener('click', event => {
-//   function restore(what, dataObject) {
-//     return function (callback) {
-//       window[what] = dataObject[`${what}Data`];
-//       storage.store(what, callback);
-//     };
-//   }
-//   if (byId('insert-data').value !== '') {
-//     const data = JSON.parse(byId('insert-data').value);
-//     const toRestore = [restore('plugins', data), restore('buttons', data)];
-//     if (!data.pluginsData) {
-//       if (!confirm('Plugin data is missing. Continue?')) toRestore.unshift();
-//     }
-//     if (!data.buttonsData) {
-//       if (!confirm('Button data is missing. Continue?')) toRestore.pop();
-//     }
-//     async.parallel(toRestore, (err, results) => {
-//       if (err) {
-//         alert(`Data restore failed: ${err}`);
-//         throw err;
-//       }
-//       alert('Data restore successful!');
-//       window.location.reload();
-//     });
-//   }
-// });
-//
-// byId('add-plugin').addEventListener('click', event => {
-//   pluginsUtil.addFromFile();
-// });
-// byId('remove-plugin').addEventListener('click', event => {
-//   pluginsUtil.promptRemoval();
-// });
-//
-// byId('add-scheme').addEventListener('click', event => {
-//   schemesUtil.addFromFile();
-// });
-// byId('remove-scheme').addEventListener('click', event => {
-//   schemesUtil.removeSelected();
-// });
-//
-// byId('add-buttons').addEventListener('click', event => {
-//   event.preventDefault();
-//   buttonsUtil.createNewButton();
-// });
-// byId('remove-buttons').addEventListener('click', event => {
-//   event.preventDefault();
-//   buttonsUtil.removeButton();
-// });
