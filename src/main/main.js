9a16b159df4eacb0605df8cdcf29612392fd09e3
Stefan Silviu <sylviu44@gmail.com>
2018-01-10 18:32:10 +0200

    Add activity system

--- src/main/main.js
+++ src/main/main.js
@@ -11,6 +11,41 @@ pluginApi.insertView = (plugin, htmlElement, order, alignment) => {
   $(`#${alignment}`).append(htmlElement);
 };
 
+class Activity {
+  constructor(name, htmlElement) {
+    if (!(htmlElement instanceof HTMLElement)) throw Error('The argument htmlElement must be of type HTMLElement');
+    this.name = name;
+    this.element = $(htmlElement);
+  }
+  show() {
+    $('.activity-title').text(this.name);
+    this.element.removeClass('hidden');
+  }
+  hide() {
+    this.element.addClass('hidden');
+  }
+}
+
+const activityStack = [
+  new Activity('New Tab', $('main.activity')[0])
+];
+
+pluginApi.pushActivity = (plugin, activityName, htmlElement) => {
+  const a = new Activity(activityName, htmlElement);
+  a.show();
+  activityStack.push(a);
+  $('.activity-up-btn').removeClass('hidden');
+};
+
+pluginApi.popActivity = plugin => {
+  if (activityStack.length === 1) return;
+  activityStack.pop().hide();
+  activityStack[activityStack.length - 1].show();
+  if (activityStack.length === 1) $('.activity-up-btn').addClass('hidden');
+};
+
+$('.activity-up-btn').click(pluginApi.popActivity);
+
 $(document).ready(() => {
   plugins.forEach(plugin => runViewContent(plugin, 'global'));
   plugins.forEach(plugin => runViewContent(plugin, 'main'));
