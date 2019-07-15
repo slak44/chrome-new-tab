b7dd30ad69877cc2cbf2558b23630c92e1c94463
Stefan Silviu <sylviu44@gmail.com>
2019-07-15 17:40:43 +0300

    Only have one page make requests in reddit plugin

--- plugins/reddit/main.js
+++ plugins/reddit/main.js
@@ -1,17 +1,35 @@
 'use strict';
 const userUrl = `https://www.reddit.com/user/${api.setting('Reddit username')}`;
+const dataKey = 'redditApiLastDataKey';
+const updateInterval = parseInt(api.setting('Data update period'), 10);
+
+const cached = JSON.parse(localStorage.getItem(dataKey));
+const initialData = {
+  commentKarma: cached === null ? '?' : cached.commentKarma,
+  linkKarma: cached === null ? '?' : cached.linkKarma,
+};
+
+const accentColor = stored.themes[stored.currentThemeIdx].accent;
 
 const html = $.parseHTML(
   `<h5>
-    <a id="comment-karma" href="${userUrl}" style="color: ${stored.themes[stored.currentThemeIdx].accent}">?</a> comment karma
+    <a id="comment-karma" href="${userUrl}" style="color: ${accentColor}">${initialData.commentKarma}</a> comment karma
     <br>
-    <a id="link-karma" href="${userUrl}" style="color: ${stored.themes[stored.currentThemeIdx].accent}">?</a> link karma
+    <a id="link-karma" href="${userUrl}" style="color: ${accentColor}">${initialData.linkKarma}</a> link karma
   </h5>`
 );
 
 api.insertView(html[0], api.setting('Order in section'), api.setting('Alignment'));
 
 setTimeout(function updateRedditKarma() {
+  const cached = JSON.parse(localStorage.getItem(dataKey));
+  if (cached !== null && Date.now() < cached.time + updateInterval) {
+    // Cache fast path
+    $('#comment-karma').text(cached.commentKarma);
+    $('#link-karma').text(cached.linkKarma);
+    setTimeout(updateRedditKarma, updateInterval);
+    return;
+  }
   $.get(`${userUrl}/about.json`, (data, textStatus, jqXHR) => {
     let commentKarma;
     let linkKarma;
@@ -23,7 +41,8 @@ setTimeout(function updateRedditKarma() {
       commentKarma = '?';
       linkKarma = '?';
     }
+    localStorage.setItem(dataKey, JSON.stringify({commentKarma, linkKarma, time: Date.now()}));
     $('#comment-karma').text(commentKarma);
     $('#link-karma').text(linkKarma);
-  }).done(() => setTimeout(updateRedditKarma, api.setting('Data update period')));
+  }).done(() => setTimeout(updateRedditKarma, updateInterval));
 }, 0);
